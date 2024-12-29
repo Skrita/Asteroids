5fd41482adca70facca3bef321985318bde26329
Skrita <gsinthar@gmail.com>
2024-12-29 16:54:14 +0100

    added score

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    score = 0
     
     drawable = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
@@ -30,27 +31,39 @@ def main():
 
     Shot.containers = (drawable, updatable,shots)
 
+    font = pygame.font.Font(None, 36)  # None uses the default font, and 36 is the font size
+
     dt = 0 # delta time - to track how much time has passed
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+
+        #score display
+        score_text = font.render(f"Score: {score}", True, (255, 255, 255))  # White color text
+        screen.blit(score_text, (10, 10))
+
+
         for drawing in drawable :
             drawing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) /1000
+
         for object in updatable :
             object.update(dt)
+
         # collision
         for asteroid in asteroids :
             if asteroid.collision(player) : 
-                print ("Game over!")
+                print (f"Game over! Final score :{score}")
                 return
             for shot in shots :
                 if asteroid.collision(shot) :
                     shot.kill()
                     asteroid.split()
+                    score+=asteroid.radius//20
+
 
            
         

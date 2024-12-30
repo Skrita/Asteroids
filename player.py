27f356ae45d86194df3d0bc532a3447a407cbc90
Skrita <gsinthar@gmail.com>
2024-12-30 12:45:26 +0100

    added engine plume

--- player.py
+++ player.py
@@ -17,8 +17,23 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
+    def engine_plume(self, direction):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + direction * forward * self.radius *1.5
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    
+    
     def draw(self, screen):
         pygame.draw.polygon(screen, "blue", self.triangle(),2)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            pygame.draw.polygon(screen, "orange", self.engine_plume(-1),1)
+        if keys[pygame.K_s]:
+            pygame.draw.polygon(screen, "orange", self.engine_plume(1),1)
     
     def rotate(self, dt):
         self.rotation+= PLAYER_TURN_SPEED*dt
